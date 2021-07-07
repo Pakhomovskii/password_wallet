878edd12889fb9aaa312c1b3441badde10363dc7
Alexander.Pakhomovskii <pakhomovskii.a@granit-concern.ru>
2021-07-07 18:59:09 +0300

    create bot

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+// password_wallet
+
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    teloxide::enable_logging!();
+    log::info!("Starting dices_bot...");
+
+    let bot = Bot::from_env().auto_send();
+
+    teloxide::repl(bot, |message| async move {
+        message.answer_dice().await?;
+        respond(())
+    })
+    .await;
+}
